9366db2bd0bfca0209bab4e72be31a1cf71a282d
Jonny <jonnyhawley@gmail.com>
2026-09-24 22:07:29 +0200

    Add the DUVATL logo, favicon and share image

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
+import logo from './assets/logo.webp'
 
 function App() {
   return (
     <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Container component="main" maxWidth="sm" sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', py: 10 }}>
+        <Box
+          component="img"
+          src={logo}
+          alt="DUVATL, FLA-GA"
+          width={657}
+          height={827}
+          sx={{ display: 'block', width: { xs: 160, sm: 200 }, height: 'auto', mb: 4 }}
+        />
         <Typography variant="h1">DUVATL, Inc.</Typography>
         <Typography sx={{ mt: 3 }}>
           We make{' '}
